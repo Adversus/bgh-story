d257320fee60601b6e679bfce4dbb4b94ea3aeca
Shaun Meyer <meyersh@morningside.edu>
2013-04-16 13:21:05 -0500

    Added initial sql for database creation.

---
+++ admin/database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE  `interactive_story` ;
+
+CREATE TABLE  `interactive_story`.`responses` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`response_text` VARCHAR( 1024 ) NOT NULL COMMENT  'Text string for a given response',
+`response_fact_id` INT NULL COMMENT  'foreign_key',
+`parent_scenario_id` INT NOT NULL COMMENT  'foreign_key',
+`response_consequence_scenario_id` INT NOT NULL COMMENT  'foreign_key'
+) ENGINE = MYISAM ;
+
