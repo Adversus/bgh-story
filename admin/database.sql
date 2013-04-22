539119558415d081051265400688c95583e818e7
Shaun Meyer <meyersh@morningside.edu>
2013-04-22 11:08:26 -0500

    Imported Mike's schema.

--- admin/database.sql
+++ admin/database.sql
@@ -6,3 +6,26 @@ CREATE TABLE  `interactive_story`.`responses` (
 `response_consequence_scenario_id` INT NOT NULL COMMENT  'foreign_key'
 ) ENGINE = MYISAM ;
 
+CREATE TABLE  `interactive_story`.`scenarios` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`story_id` INT NOT NULL COMMENT  'foreign_key',
+`scenario_body_id` INT NOT NULL COMMENT  'foreign_key'
+) ENGINE = MYISAM ;
+
+CREATE TABLE  `interactive_story`.`stories` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`story_name` VARCHAR( 50 ) NOT NULL COMMENT  'Text string for a story name',
+`start_screen_body_id` INT NOT NULL COMMENT  'foreign_key',
+`end_screen_body_id` INT NOT NULL COMMENT  'foreign_key',
+`first_scenario_id` INT NOT NULL COMMENT  'foreign_key'
+) ENGINE = MYISAM ;
+
+CREATE TABLE  `interactive_story`.`facts` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`fact_body` VARCHAR( 1024 ) NOT NULL COMMENT  'Text string for a given fact'
+) ENGINE = MYISAM ;
+
+CREATE TABLE  `interactive_story`.`bodies` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`text` VARCHAR( 1024 ) NOT NULL COMMENT  'Text string for any given text'
+) ENGINE = MYISAM ;
