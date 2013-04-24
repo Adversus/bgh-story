076bf8b988cba5efcadeab6977c4af4e590856e9
Shaun Meyer <meyersh@morningside.edu>
2013-04-24 16:12:20 -0500

    Commiting loadStory.php

---
+++ loadStory.php
@@ -0,0 +1,32 @@
+<?PHP
+header('Content-type: application/json');
+
+include("admin/database.php");
+
+if (!isset($_GET["story_id"])){
+  $story_id = 1;
+ }
+ else {
+   $story_id = $_GET["story_id"];
+ }
+
+if (isValidStory($story_id)) {
+
+
+
+$story_scenarios = getStoryScenarios($story_id);
+
+print json_encode(array("scenarios"      => $story_scenarios,
+                        "facts"          => getFactsUsedInStory($story_id),
+                        "start_screen"   => getStoryStartText($story_id),
+                        "end_screen"     => getStoryEndText($story_id),
+                        "start_scenario" => getStoryStartScenario($story_id)));
+
+ }
+ else{
+
+   print json_encode(array("error" => "INVALID_STORY"));
+
+}
+
+?>
