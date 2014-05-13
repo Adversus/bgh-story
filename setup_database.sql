b674447d163027caf7865d519ccbdec675ff695d
Donald Franklin <df0004@morningside.edu>
2014-05-13 13:04:27 -0500

    Extend schema for new feature (sound effects)

--- setup_database.sql
+++ setup_database.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS `BGHinteractive`.`boxes` (
 `story_id` INT NOT NULL COMMENT  'foreign_key',
 `title` VARCHAR( 30 ) NOT NULL COMMENT  'Text string for a box title',
 `text` VARCHAR( 1024 ) NOT NULL COMMENT  'Text string for any given text',
+`sound_id` INT NOT NULL COMMENT  'foreign_key',
 `x` INT NOT NULL COMMENT  'horizontal position on the graph',
 `y` INT NOT NULL COMMENT  'vertical position on the graph',
 `grad_primary` VARCHAR( 7 ) NULL COMMENT  'Hex color code for the gradient bg',
@@ -20,6 +21,13 @@ CREATE TABLE IF NOT EXISTS `BGHinteractive`.`choices` (
 `story_id` INT NOT NULL COMMENT  'foreign_key',
 `choice` VARCHAR( 100 ) NOT NULL COMMENT  'Text string for a choice display',
 `fact` VARCHAR( 1024 ) NOT NULL COMMENT  'Text string for a given fact',
+`sound_id` INT NOT NULL COMMENT  'foreign_key',
 `box1_id` INT NOT NULL COMMENT  'foreign_key',
 `box2_id` INT NOT NULL COMMENT  'foreign_key'
+) ENGINE = MYISAM ;
+
+CREATE TABLE IF NOT EXISTS `BGHinteractive`.`sounds` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`name` VARCHAR( 100 ) NOT NULL COMMENT  'Text string for a menu display name',
+`url` VARCHAR( 1024 ) NOT NULL COMMENT  'Text string for the url of the sound'
 ) ENGINE = MYISAM ;
