bc0fdd8e4a5d92db306414a735543837b979cc61
Donald Franklin <df0004@morningside.edu>
2014-05-13 00:46:50 -0500

    Update to use new story update conversion method

--- storyupdate.php
+++ storyupdate.php
@@ -15,24 +15,5 @@ if ($page > 0){
 	return 0;
 }
 
-if (count($story_boxes) > 0){
-	$obj = new stdClass();
-	$obj->text = $story_boxes[0]->Text;
-	$obj->grad1 = $story_boxes[0]->grad1;
-	$obj->grad2 = $story_boxes[0]->grad2;
-	$obj->sound = "";
-	$obj->choices = Array();
-	
-	if (count($story_choices) > 0){
-		foreach ($story_choices as $choice){
-			$newChoice = new stdClass();
-			$newChoice->Choice = $choice->Choice;
-			$newChoice->Fact = $choice->Fact;
-			$newChoice->Target = $choice->Box2;
-			array_push($obj->choices, $newChoice);
-		}
-	}
-	
-	print json_encode($obj);
-}
+print getStoryUpdate();
 ?>
