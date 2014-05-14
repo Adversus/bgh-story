94b4b2705410d8e3d5a33c60749c8adef682a2c3
Donald Franklin <df0004@morningside.edu>
2014-05-14 01:40:05 -0500

    Remove sending default graph on save

--- editor/save.php
+++ editor/save.php
@@ -1,9 +1,6 @@
 <?PHP
 include("database.php");
 
-//** Set default graph
-$input = "-1{B,-1,Start,(Starting message),150,200}{B,-2,End,(Completion message),550,200}{L,-1,(new choice),,-1,-2}";
-
 //** If a graph was sent from client than the default graph gets replaced
 if(isset($_POST['graph']) && !empty($_POST['graph'])) {
     $input = $_POST['graph'];
@@ -11,7 +8,7 @@ if(isset($_POST['graph']) && !empty($_POST['graph'])) {
 	saveStory();
 	sendGraphObjects();
 } else {
-	//** No graph was sent so send default
-	print ($input);
+	//** No graph was sent so send nothing
+	print ("");
 }
 ?>
